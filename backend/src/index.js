5a95a151e9aa2791e055298442312d9d3ee2a3c5
XOB <titanuzbgamer@gmail.com>
2026-06-06 18:36:25 +0500

    backend indexjsga like ni qoshdi

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,12 +15,15 @@ app.use(cors());
 const authRoutes = require('./routes/auth.routes');
 const korzinkaRoutes = require('./routes/korzinka.routes');
 const getAll_create = require("./routes/get_create.routes")
+const likeRoutes = require("./routes/like.routes")
 
 
 
 app.use(authRoutes);
 app.use(korzinkaRoutes);
 app.use(getAll_create)
+app.use(likeRoutes)
+
 
 app.listen(process.env.PORT, () => {
     console.log(`server run http://localhost:${process.env.PORT}`);
